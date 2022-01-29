8d487a9bf355df8751d3a724a83c28defe4c16b3
Dire Akanbi <direakanbi@icloud.com>
2022-01-29 20:01:06 +0100

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,14 @@
-import { Link } from 'react-router-dom';
-import id from './C16A2340-7BE6-4ACE-88D5-E6DF9E85165C.png';
- const Nav = () => (
+import { Link } from "react-router-dom";
+import id from "./C16A2340-7BE6-4ACE-88D5-E6DF9E85165C.png";
+const Nav = () => (
   <div className="nav">
     <nav className="same">
       <h1 className="heading">Bookstore CMS</h1>
-       <ul className="nav-link">
+      <ul className="nav-link">
         <Link to="/">BOOKS</Link>
         <Link to="/categories">CATEGORIES</Link>
-       </ul>
-     </nav>
+      </ul>
+    </nav>
     <img src={id} alt="id" width="40px" height="40px" />
   </div>
 );
